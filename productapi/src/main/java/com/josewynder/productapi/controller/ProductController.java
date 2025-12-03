b5b1944c47921d695654dfa5ae7372f0a7847bfc
José Wynder <wynder.jose@gmail.com>
2025-12-03 15:00:29 -0300

    lesson(2.10): Creating an endpoint for receiving products

---
+++ productapi/src/main/java/com/josewynder/productapi/controller/ProductController.java
@@ -0,0 +1,18 @@
+package com.josewynder.productapi.controller;
+
+import com.josewynder.productapi.model.Product;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        System.out.println("Product received: " + product);
+        return product;
+    }
+}
