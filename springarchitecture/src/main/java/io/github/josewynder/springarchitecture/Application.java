30b4727125a724b7202b6bdc65bf97a0a8689315
Jose Wynder <wynder.jose@gmail.com>
2025-12-16 18:13:31 -0300

    lesson(3.23): Understanding the application class

---
+++ springarchitecture/src/main/java/io/github/josewynder/springarchitecture/Application.java
@@ -0,0 +1,32 @@
+package io.github.josewynder.springarchitecture;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(Application.class, args);
+
+		SpringApplicationBuilder builder =
+				new SpringApplicationBuilder(Application.class);
+
+		builder.profiles("production", "approval");
+		builder.run(args);
+
+		// Application context already started.
+		ConfigurableApplicationContext applicationContext = builder.context();
+//		var productRepository = applicationContext.getBean("productRepository");
+
+
+		ConfigurableEnvironment environment = applicationContext.getEnvironment();
+		String applicationName = environment.getProperty("spring.application.name");
+		System.out.println("Application Name: " + applicationName);
+	}
+
+}
